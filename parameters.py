98c82e08d58863c7c75eaec92e55166cc7d89310
Roy van Bragt <roy@van-bragt.com>
2020-11-04 15:39:26 +0100

    Removed old files.

--- parameters.py
+++
@@ -1,8 +0,0 @@
-# This file serves to store the parameters for each model in.
-# It has its own separate file to prevent clutter in the scripts.
-
-rf_params = {
-    'n_estimators':1000,
-    'criterion':'mse',
-    'max_depth': None,
-}
